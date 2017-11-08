7a543af3f517a311aa0f9e4b7611bc8edf815f03
Psycoos <coospot4@gmail.com>
2017-11-08 13:41:32 +0100

    added button for nfc scanning with notification text

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -19,6 +19,7 @@ export class HomePage {
   isWriting:    boolean   = false;
   ndefMsg:      string    = '';
   subscriptions: Array<Subscription> = new Array<Subscription>();
+  message = '';
 
   constructor(
     public platform: Platform,
@@ -65,8 +66,14 @@ export class HomePage {
     });
   }
 
+
+
+
   readTag() {
     this.readingTag = true;
+    if (this.readingTag == true){
+      this.message = "Scan nu een NFC tag";
+    }
   }
 
   writeTag(writeText: string) {
